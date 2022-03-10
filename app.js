442c40c69cd0533fc987974cf9c7a54e748b8e2b
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2022-03-09 20:17:39 -0800

    Added iPhone 13 project

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 console.clear();
-/* The encoding is super important here to enable frame-by-frame scrubbing. */
 
+/* Video encoding to enable frame-by-frame scrubbing. */
 // ffmpeg -i ~/Downloads/Toshiba\ video/original.mov -movflags faststart -vcodec libx264 -crf 23 -g 1 -pix_fmt yuv420p output.mp4
 // ffmpeg -i ~/Downloads/Toshiba\ video/original.mov -vf scale=960:-1 -movflags faststart -vcodec libx264 -crf 20 -g 1 -pix_fmt yuv420p output_960.mp4
 
 const video = document.querySelector(".video-background");
 let src = video.currentSrc || video.src;
-// console.log(video, src);
 
-/* Make sure the video is 'activated' on iOS */
+/* Make sure the video is 'activated' on iOS devices on low-battery mode */
 function once(el, event, fn, opts) {
   var onceFn = function (e) {
     el.removeEventListener(event, onceFn);
@@ -24,9 +23,7 @@ once(document.documentElement, "touchstart", function (e) {
 });
 
 /* ---------------------------------- */
-/* Scroll Control! */
-
-gsap.registerPlugin(ScrollTrigger);
+/* Intro video scrolling */
 
 let tl = gsap.timeline({
   defaults: { duration: 1 },
@@ -40,26 +37,7 @@ let tl = gsap.timeline({
 });
 
 once(video, "loadedmetadata", () => {
-  console.log('loaded');
-  tl.fromTo(
-    video,
-    {
-      currentTime: 0
-    },
-    {
-      currentTime: video.duration || 1
-    }
-  );
+  tl.fromTo(video, { currentTime: 0 }, { currentTime: video.duration || 1 });
 });
-
-var t = video.currentTime;
-// once(document.documentElement, "touchstart", function (e) {
-//   video.play();
-//   video.pause();
-// });
-
-video.setAttribute("src", src);
-video.currentTime = t + 0.01;
-
 
 /* ---------------------------------- */
